8729a146f798b66803156e206cb54b5a2ae37646
ezybg7 <ezybg7@umsystem.edu>
2023-04-23 22:44:21 -0500

    Init
    Added basic file structure as well as simple queries for recipe to allow access to API

---
+++ hook/recipeFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const recipeFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch/${endpoint}`,
+        headers: {
+            'content-type': 'application/octet-stream',
+            'X-RapidAPI-Key': '8f883f02b5msh7293a7fb3c71a5bp18dc90jsn04765091ee8c',
+            'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+        },
+        params: { ...query },
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try{
+            const response = await axios.request(options);
+
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert('There is an error')
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch};
+}
+ 
+export default recipeFetch;
